a936ca415a8ad26111f7f2820cad14e974f743f6
Tyler Simko <tsimko@princeton.edu>
2014-09-22 13:46:41 -0400

    Initial commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -20,6 +20,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
 
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.redColor()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+    //code will evaluate when we press button
 }
 
